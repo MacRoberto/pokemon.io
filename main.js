2bb421fd8601a9f682f93676b86f23c4120f9117
MacRoberto <roberto-may-c@outlook.es>
2025-06-26 02:42:04 -0500

    Js limpieza

--- main.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  document.getElementById("mainMenu").classList.toggle("hidden");
-}
-
-function toggleSubmenu() {
-  document.getElementById("appearanceOptions").classList.toggle("hidden");
-}
